9660424c797abac131c56b98640733b0b3c596ab
Dylan Brewer <brew4934@kettering.edu>
2020-10-31 17:26:06 -0500

    making run/plot build script

---
+++ build_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Build executable
+g++ -Iinclude/ src/main.cpp src/geometry.cpp src/node.cpp -o bin/main
+
+# Run executable
+./bin/main
+
+# Plot result
+gnuplot <<- EOF
+    set term x11 persist
+    set pm3d map
+    set view map
+    splot "output/mesh.out" using 1:2:3 with image
+    pause -1
+EOF
