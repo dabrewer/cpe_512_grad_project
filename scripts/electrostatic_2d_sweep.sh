f5f88edc34eed6fb70bf67efe7fe7c6795a1b8a3
dylan <dylanabrewer@gmail.com>
2020-11-21 21:26:09 -0600

    Update method of stat logging

--- scripts/electrostatic_2d_sweep.sh
+++ scripts/electrostatic_2d_sweep.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
 
+####################################################################################################
+# DEFINITIONS
+####################################################################################################
+# Run characteristics
 ITERATION_RANGE={1..10}
 THREAD_RANGE="1 2 4 8 16 32"
 SIZE_RANGE="2.0 1.5 1.0 0.75 0.5 0.25"
 
-# Run the serial configuration 10 times for each size configuration
-for i in $ITERATION_RANGE
-do
-    for s in $SIZE_RANGE
-    do
-        ./bin/main_serial $CFG_PATH/mesh_${s}.cfg $CFG_PATH/boundary.cfg $CFG_PATH/sor.cfg $OUT_PATH/mesh_${s}.out $OUT_PATH/stat_serial_${s}.out
-    done
-done
-
-# Run each OMP thread/size configuration 10 times
-for i in $ITERATION_RANGE
-do
-    for t in $THREAD_RANGE
+####################################################################################################
+# PROGRAM EXECUTION FUNCTIONS
+####################################################################################################
+# Function to run the serial configuration 10 times for each size configuration
+run_serial()
+{
+    for i in $ITERATION_RANGE
     do
         for s in $SIZE_RANGE
         do
-            ./bin/main $t $CFG_PATH/mesh_${s}.cfg $CFG_PATH/boundary.cfg $CFG_PATH/sor.cfg $OUT_PATH/mesh_${s}.out $OUT_PATH/stat_${t}_${s}.out
+            ./bin/main_serial $CFG_PATH/mesh_${s}.cfg $CFG_PATH/boundary.cfg $CFG_PATH/sor.cfg $OUT_PATH/mesh_serial_${s}.out $OUT_PATH/stat_serial.out
         done
     done
-done
+}
 
-# TODO: calculate average of $OUT_PATH/stat_${t}_${s}.out
+# Function to run each OMP thread/size configuration 10 times
+run_omp()
+{
+    for i in $ITERATION_RANGE
+    do
+        for t in $THREAD_RANGE
+        do
+            for s in $SIZE_RANGE
+            do
+                ./bin/main $t $CFG_PATH/mesh_${s}.cfg $CFG_PATH/boundary.cfg $CFG_PATH/sor.cfg $OUT_PATH/mesh_${t}_${s}.out $OUT_PATH/stat_parallel.out
+            done
+        done
+    done
+}
+
+####################################################################################################
+# MAIN ROUTINE
+####################################################################################################
+run_serial
+run_omp
--- src/main.cpp
+++ src/main.cpp
@@ -98,7 +98,7 @@ int main( int argc, char *argv[] )
 
     // Append run statistics to output file
     statFile.open(FNAME_STAT_OUT, ios_base::app);
-    statFile << TIMER_ELAPSED << endl; 
+    statFile << NUM_THREADS << "," << mesh->getNumNodes() << "," << iterations << "," << TIMER_ELAPSED << endl; 
 
     delete mesh;
 }
