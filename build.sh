97e43da500e991894ad23c5e7a17989e252d0705
dylan <dylanabrewer@gmail.com>
2020-11-21 21:35:46 -0600

    Update build script to only build, not run

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+CFG_PATH=config
+OUT_PATH=output
+BIN_PATH=bin
+
+# Ensure the output directories exist
+mkdir -p $OUT_PATH
+mkdir -p $BIN_PATH
+
+# Build executable
+g++ -Isrc/include/ src/main.cpp src/mesh.cpp src/node.cpp -o bin/main -fopenmp
