74fe4c0218d5d8d77517e14635f9be4375779c3a
dylan <dylanabrewer@gmail.com>
2020-11-21 21:29:47 -0600

    Update stat logging for serial program

--- src/main.cpp
+++ src/main.cpp
@@ -89,7 +89,7 @@ int main( int argc, char *argv[] )
 
     // Append run statistics to output file
     statFile.open(FNAME_STAT_OUT, ios_base::app);
-    statFile << mesh->getNumNodes() << " " << TIMER_ELAPSED << endl; 
+    statFile << 1 << "," << mesh->getNumNodes() << "," << iterations << "," << TIMER_ELAPSED << endl;  
 
     delete mesh;
 }
