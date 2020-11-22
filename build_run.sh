4a5a807184d06dfde7b48a519478f3b554dd84e2
dylan <dylanabrewer@gmail.com>
2020-11-21 21:32:48 -0600

    Update name of executable

--- build_run.sh
+++ build_run.sh
@@ -8,16 +8,4 @@ mkdir -p $OUT_PATH
 mkdir -p $BIN_PATH
 
 # Build executable
-g++ -Isrc/include/ src/main.cpp src/mesh.cpp src/node.cpp -o bin/main
-
-# Run executable
-./bin/main $CFG_PATH/mesh.cfg $CFG_PATH/boundary.cfg $CFG_PATH/sor.cfg $OUT_PATH/mesh.out $OUT_PATH/stat.out
-
-# Plot result
-gnuplot <<- EOF
-    set term x11 persist
-    set pm3d map
-    set view map
-    splot "output/mesh.out" using 1:2:3 with image
-    pause -1
-EOF
+g++ -Isrc/include/ src/main.cpp src/mesh.cpp src/node.cpp -o bin/main_serial
