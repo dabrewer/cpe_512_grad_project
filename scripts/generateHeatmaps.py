cc7e3dc01daad738c8e0eaa46c24a93fd6c74819
dylan <dylanabrewer@gmail.com>
2020-12-01 22:31:45 -0600

    Update heatmap titles, add commented code to plot mesh grid

--- scripts/generateHeatmaps.py
+++ scripts/generateHeatmaps.py
@@ -4,12 +4,15 @@ import glob
 import numpy as np
 import matplotlib.pyplot as plt
 
-PLOT_TITLE = "Mesh Potentials (V): Threads = {}, Size = {}"
+#PLOT_TITLE = "Mesh Potentials (V): Threads = {}, Size = {} cm"
+PLOT_TITLE = "Mesh Potentials (V): Size = {} cm"
 
 def plot(x, y, v, threads, size):
     plt. clf()
     
-    plt.title(PLOT_TITLE.format(threads, size))
+    #plt.title(PLOT_TITLE.format(threads, size))
+
+    plt.title(PLOT_TITLE.format(size))
 
     x=np.unique(x)
     y=np.unique(y)
@@ -18,6 +21,9 @@ def plot(x, y, v, threads, size):
 
     mesh = plt.pcolormesh(X,Y,V, cmap='jet')
     plt.colorbar(mesh)
+
+    # [plt.hlines(y=yVal, xmin=0.0, xmax=65.0, linewidth=0.5) for yVal in np.arange(0, 50, float(size))]
+    # [plt.vlines(x=xVal, ymin=0.0, ymax=50.0, linewidth=0.5) for xVal in np.arange(0, 65, float(size))]
 
     plt.savefig("output/heatmaps/mesh_{}_{}.png".format(threads, size))
 
